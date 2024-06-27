d5bb4bc55ba8e30c68d413d75e3d6100f481e2a9
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-27 14:24:19 -0300

    Modifiquei algumas linhas.

--- src/test/java/org/desafio_api/ViaCepTeste.java
+++ src/test/java/org/desafio_api/ViaCepTeste.java
@@ -5,17 +5,19 @@ import org.apache.http.HttpStatus;
 import org.desafio_api.supports.BaseTest;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class ViaCepTeste extends BaseTest {
 
     @Test
     public void validZipCodeTest() {
-        RestAssured.given()
-                .when()
-                    .get("/92440094/json")
-
-                .then()
-                    .log().body()
-                    .statusCode(HttpStatus.SC_OK);
+        given()
+            .spec(requestSpec)
+        .when()
+            .get("92440094/" + JSON)
+        .then()
+            .log().body()
+            .statusCode(HttpStatus.SC_OK);
     }
 
 }
