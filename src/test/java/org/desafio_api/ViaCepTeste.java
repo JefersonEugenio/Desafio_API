09b2ae7985c21f9a88afc1026efe6591f0b1233c
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-27 15:49:41 -0300

    Importei fakers e coloquei spec e fake cep.

--- src/test/java/org/desafio_api/ViaCepTeste.java
+++ src/test/java/org/desafio_api/ViaCepTeste.java
@@ -1,21 +1,36 @@
 package org.desafio_api;
 
-import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.desafio_api.supports.BaseTest;
+import org.desafio_api.supports.Fakers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class ViaCepTeste extends BaseTest {
 
+    private Fakers fakers = new Fakers();
+
     @Test
     public void validZipCodeTest() {
         given()
             .spec(requestSpec)
         .when()
-            .get("92440094/" + JSON)
+            .get("92440094" + JSON)
         .then()
+            .spec(responseSpecJson)
+            .log().body()
+            .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void validZipCode2Test() {
+        given()
+            .spec(requestSpec)
+        .when()
+            .get( fakers.getCode() + JSON)
+        .then()
+            .spec(responseSpecJson)
             .log().body()
             .statusCode(HttpStatus.SC_OK);
     }
