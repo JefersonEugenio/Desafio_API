fdea04553f76d176d927963b90d50ff7c6d998f5
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-27 16:23:58 -0300

    Obter properties cep PUC.

--- src/main/java/org/desafio_api/supports/EndPoints.java
+++ src/main/java/org/desafio_api/supports/EndPoints.java
@@ -5,6 +5,7 @@ import org.desafio_api.utils.ObjectsUtils;
 public class EndPoints {
 
     protected static final String BASE_URI = ObjectsUtils.getPropertiesData("endpoints", "BASE_URI");
+    protected static final String CEP_PUCRS = ObjectsUtils.getPropertiesData("endpoints", "CEP_PUCRS");
     protected static final String JSON = ObjectsUtils.getPropertiesData("endpoints", "JSON");
 
 }
