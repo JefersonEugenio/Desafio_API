361eda3b80892edeb09e2bf96e07ee633bfbc741
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-27 12:30:43 -0300

    Modifiquei link para properties obter URI.

--- src/main/java/org/desafio_api/supports/BaseTest.java
+++ src/main/java/org/desafio_api/supports/BaseTest.java
@@ -1,13 +1,14 @@
 package org.desafio_api.supports;
 
 import io.restassured.RestAssured;
+import org.desafio_api.utils.ObjectsUtils;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
 
     @BeforeEach
     public void setup() {
-        RestAssured.baseURI = "https://viacep.com.br/ws";
+        RestAssured.baseURI = ObjectsUtils.getPropertiesData("endpoints", "BASE_URI");
     }
 
 }
