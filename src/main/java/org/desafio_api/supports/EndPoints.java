6dbf10f0f0215709edb5115c25362c0f09060503
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-27 12:29:49 -0300

    Criei classe EndPoints e obter properties URI

--- src/main/java/org/desafio_api/supports/EndPoints.java
+++ src/main/java/org/desafio_api/supports/EndPoints.java
@@ -1,2 +1,9 @@
-package org.desafio_api.supports;public class EndPoints {
+package org.desafio_api.supports;
+
+import org.desafio_api.utils.ObjectsUtils;
+
+public class EndPoints {
+
+    protected static final String BASE_URI = ObjectsUtils.getPropertiesData("endpoints", "BASE_URI");
+
 }
