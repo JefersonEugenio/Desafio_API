af78ed9e49c619ea4e4c662d04e1ac9c33db59a5
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-28 14:56:47 -0300

    Obter properties valor 'EXTRA'.

--- src/main/java/org/desafio_api/supports/EndPoints.java
+++ src/main/java/org/desafio_api/supports/EndPoints.java
@@ -7,5 +7,6 @@ public class EndPoints {
     protected static final String BASE_URI = ObjectsUtils.getPropertiesData("endpoints", "BASE_URI");
     protected static final String CEP_PUCRS = ObjectsUtils.getPropertiesData("endpoints", "CEP_PUCRS");
     protected static final String JSON = ObjectsUtils.getPropertiesData("endpoints", "JSON");
+    protected static final String EXTRA = ObjectsUtils.getPropertiesData("endpoints", "EXTRA");
 
 }
